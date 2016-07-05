a7b5209fecfc5623c039238a1f93143076ea1c98
Kristina <isitiriss@gmail.com>
2016-07-05 14:57:22 +0300

    Cosmetic changes
    Added configlogfile

--- main.go
+++ main.go
@@ -3,20 +3,11 @@ package main
 import _ "github.com/KristinaEtc/slflog"
 
 import (
-	//"fmt"
 	auth "github.com/KristinaEtc/auth/auth"
-	//dAuth "github.com/abbot/go-http-auth"
 	gin "github.com/gin-gonic/gin"
 	"github.com/ventu-io/slf"
 	webauth "tekinsoft/web"
 )
-
-func Secret(user, realm string) string {
-	if user == "john" {
-		return "b98e16cbc3d01734b264adba7baa3bf9"
-	}
-	return ""
-}
 
 func main() {
 
@@ -26,14 +17,15 @@ func main() {
 	webauth.ConfigureFromFile("./webauth.json")
 	r := gin.New()
 
-	r.Use(auth.MultiAuthMiddleware())
-	r.Use(auth.BasicMiddleware())
-	r.Use(auth.DigestMiddleware())
-	r.Use(auth.MiddleSecond())
+	r.Use(auth.MultiAuthMiddleware(),
+		auth.BasicMiddleware(),
+		auth.DigestMiddleware(),
+		auth.MiddlewareSecond(),
+	)
 
 	r.GET("/status", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "test",
 		})
 	})
 
