a9b1f6793a05465907d9c2d43a871b41acc95da0
Kristina <isitiriss@gmail.com>
2016-07-13 20:16:41 +0300

    working cookies authorization with basic css-form

--- example/cookie2.go
+++ example/cookie2.go
@@ -1,91 +1,86 @@
-/*package main
-
-import (
-	"github.com/gin-gonic/contrib/sessions"
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	r := gin.Default()
-	store := sessions.NewCookieStore([]byte("secret"))
-	r.Use(sessions.Sessions("mysession", store))
-
-	r.GET("/incr", func(c *gin.Context) {
-		session := sessions.Default(c)
-		var count int
-		v := session.Get("count")
-		if v == nil {
-			count = 0
-		} else {
-			count = v.(int)
-			count += 1
-		}
-		session.Set("count", count)
-		session.Save()
-		c.JSON(200, gin.H{"count": count})
-	})
-	r.Run(":8000")
-}
-*/
-
 package main
 
+import _ "github.com/KristinaEtc/slflog"
+
 import (
-	"fmt"
+	auth "github.com/KristinaEtc/auth"
 	"github.com/gin-gonic/contrib/sessions"
 	gin "github.com/gin-gonic/gin"
+	"github.com/ventu-io/slf"
+	"net/http"
 )
 
+var log slf.StructuredLogger
+
 func loginHandler(c *gin.Context) {
-	request := c.Request
-	name := request.FormValue("name")
-	pass := request.FormValue("password")
+
+	name := c.Request.FormValue("name")
+	pass := c.Request.FormValue("password")
 
 	if name != "" && pass != "" {
-		fmt.Println(name, pass)
+		log.Debugf("name=%s, pass=%s\n", name, pass)
 		session := sessions.Default(c)
 		session.Set("name", name)
+		session.Set("password", pass)
 		session.Save()
-		c.JSON(200, gin.H{"name": name})
-
+		//c.HTML(200, "index.html", nil)
+	} else {
+		//should never happend: was checked by html
+		log.WithFields(slf.Fields{"name=": name,
+			"password=": pass,
+		}).Warnf("User authorization with emply field")
 	}
 }
 
 func indexPageHandler(c *gin.Context) {
-
-	c.HTML(200, "index3.html", nil)
-
+	c.HTML(200, "index.html", nil)
 }
 
 //func internalPageHandler(response http.ResponseWriter, request *http.Request) {
-func internalPageHandler(c *gin.Context) {
+/*func CheckCookies() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-	session := sessions.Default(c)
-	userName := session.Get("name")
-
-	if userName != "" {
-		//fmt.Fprintf(response, internalPage, userName)
-		fmt.Println(userName)
-	} else {
-		fmt.Println("FUCK")
-		//http.Redirect(response, request, "/", 302)
 	}
-}
+}*/
 
 func main() {
 
+	log = slf.WithContext("auth-main.go")
+	log.Info("test")
+
+	auth.ConfigureFromFile("./webauthExample.json")
+
+	r := gin.Default()
+	r.Use(gin.Logger())
+
 	store := sessions.NewCookieStore([]byte("authStore"))
+	r.Use(sessions.Sessions("Authorization", store))
 
-	router := gin.Default()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.html")
-	router.Use(sessions.Sessions("Authorization", store))
+	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/static", "static")
 
-	router.GET("/", indexPageHandler)
-	router.GET("/internal", internalPageHandler)
+	r.GET("/", indexPageHandler)
 
-	router.POST("/login", loginHandler)
-	//	router.POST("/logout", logoutHandler)
+	r.POST("/monitoring", func(c *gin.Context) {
+		defer log.WithFields(slf.Fields{"func": "CheckCookies"})
+		session := sessions.Default(c)
+		userName := session.Get("name")
 
-	router.Run(":8000")
+		if userName != "" {
+			//fmt.Fprintf(response, internalPage, userName)
+			log.Debug(userName.(string))
+			c.HTML(http.StatusOK, "index3.html", nil)
+		} else {
+			log.Debug("FUCK")
+			//http.Redirect(response, request, "/", 302)
+		}
+
+	})
+	//r.Use(CheckCookies())
+	//r.GET("/monitoring", internalPageHandler)
+
+	r.POST("/login", loginHandler)
+	//	r.POST("/logout", logoutHandler)
+
+	r.Run(":8000")
 }
