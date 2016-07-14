b6d0e41bf45edd8492c51767f62f9731039c7921
Kristina <isitiriss@gmail.com>
2016-07-14 14:16:17 +0300

    a dirty working version of cookies-form authorization

--- example/main.go
+++ example/main.go
@@ -4,37 +4,65 @@ import _ "github.com/KristinaEtc/slflog"
 
 import (
 	auth "github.com/KristinaEtc/auth"
+	//"github.com/gin-gonic/contrib/sessions"
 	gin "github.com/gin-gonic/gin"
 	"github.com/ventu-io/slf"
+	//"net/http"
+	"github.com/gin-gonic/contrib/sessions"
 )
+
+var log slf.StructuredLogger
 
 func main() {
 
-	log := slf.WithContext("auth-main.go")
+	log = slf.WithContext("auth-main.go")
 	log.Info("test")
 
 	auth.ConfigureFromFile("./webauthExample.json")
-	r := gin.New()
 
-	r.Use(auth.MultiAuthMiddleware(),
+	r := gin.Default()
+	r.Use(gin.Logger())
+
+	//r.NoRoute(redirect)
+
+	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/static", "static")
+
+	monitoring := r.Group("/")
+
+	store := sessions.NewCookieStore([]byte("authStore"))
+	monitoring.Use(sessions.Sessions("Authorization", store))
+	r.Use(sessions.Sessions("Authorization", store))
+
+	monitoring.Use(auth.MultiAuthMiddleware(),
 		auth.TrustMiddleware(),
 		auth.BasicAuthMiddleware(),
 		auth.DigestAuthMiddleware(),
 		auth.MiddlewareSecond(),
+		auth.CookieMiddleware(),
 	)
-
-	r.GET("/status", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "status",
+	{
+		monitoring.POST("/monitoring", func(c *gin.Context) {
+			c.HTML(200, "monitoring.html", nil)
 		})
-	})
-
-	r.GET("/monitoring", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "monitoring",
+		monitoring.GET("/", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "/",
+			})
 		})
-	})
+		monitoring.GET("/status", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "status",
+			})
+		})
+		monitoring.GET("/monitoring", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "monitoring",
+			})
+		})
+	}
 
-	// Listen and server on 0.0.0.0:8080
+	r.POST("/login", auth.LoginHandler)
+
 	r.Run(":8080")
 }
